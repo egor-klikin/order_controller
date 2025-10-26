5f3f263a3538450cfc7bda4bb754b4be26466600
egor-klikin <rikirokorok@gmail.com>
2025-10-26 12:12:19 +0300

    Добавление тестов

---
+++ OrderModuleTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace OrderModuleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
